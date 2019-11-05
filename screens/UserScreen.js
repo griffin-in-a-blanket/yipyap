2e17e95d663a415b5d60fdc3e37843b5f4fcf0d8
Danny Li <49844313+dongledan@users.noreply.github.com>
2019-11-05 14:21:27 -0500

    added new tabs and changed tint color (#1)

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 
-export default function SettingsScreen() {
+export default function UserScreen() {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  return <Text> Cats are evil </Text>
 }
 
-SettingsScreen.navigationOptions = {
+UserScreen.navigationOptions = {
   title: 'app.json',
 };
