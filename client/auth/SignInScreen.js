8ab3a01159112a6f0da1faaae5f62d36bfd48d0e
Danny Li <49844313+dongledan@users.noreply.github.com>
2019-11-06 14:59:25 -0500

    Firebase (#3)
    * added Post skeleton
    
    * setup firebase
    
    * added auth with sign in/signup/forgot password

---
+++ client/auth/SignInScreen.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import * as firebase from "firebase";
+
+import { firebaseConfig } from '../../secrets';
+import Styles from '../constants/Styles';
+
+firebase.initializeApp(firebaseConfig);
+
+export default class SignInScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  onSignInPress = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(
+        () => {
+          this.props.navigation.navigate("Home");
+        },
+        error => {
+          Alert.alert(error.message);
+        }
+      );
+  };
+
+  onSignUpPress = () => {
+    this.props.navigation.navigate("SignUp");
+  };
+
+  onGuestPress = () => {
+    this.props.navigation.navigate("Home");
+  };
+
+  onForgotPasswordPress = () => {
+    this.props.navigation.navigate("ForgotPassword");
+  };
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <View style ={Styles.container}>
+          <TextInput
+            placeholder="email"
+            placeholderTextColor="rgba(255, 255, 255, 0.8)"
+            returnKeyType="next"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            onSubmitEditing={() => this.passwordInput.focus()}
+            value={this.state.email}
+            onChangeText={text => this.setState({ email: text })}
+            style={Styles.inputText}
+          />
+          <TextInput
+            placeholder="password"
+            placeholderTextColor="rgba(255, 255, 255, 0.8)"
+            secureTextEntry
+            returnKeyType="go"
+            ref={input => (this.passwordInput = input)}
+            value={this.state.password}
+            onChangeText={text => this.setState({ password: text })}
+            style={Styles.inputText}
+          />
+        </View>
+        <View style={Styles.buttonContainer}>
+          <TouchableOpacity onPress={this.onSignInPress}>
+            <Text style={Styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onSignUpPress}>
+            <Text style={Styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onGuestPress}>
+            <Text style={Styles.buttonText}>Guest</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.onForgotPasswordPress}>
+            <Text style={Styles.buttonText}>Forgot your password ?</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
--- client/navigation/AppNavigator.js
+++ client/navigation/AppNavigator.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import StartScreen from '../screens/StartScreen';
+import SignInScreen from '../auth/SignInScreen';
+import SignUpScreen from '../auth/SignUpScreen';
+import ForgetPasswordScreen from '../auth/ForgotPasswordScreen';
+
+const Auth = createStackNavigator(
+  {
+    Start: StartScreen,
+    SignIn: SignInScreen,
+    SignUp: SignUpScreen,
+    ForgotPassword: ForgetPasswordScreen,
+  }
+)
 
 export default createAppContainer(
   createSwitchNavigator({
+    Auth,
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
